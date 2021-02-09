8cdef2cab68088421efe5bf9aff0843dcac213ea
pranav-geolabs <pranav@geometrylabs.io>
2021-02-09 13:27:44 -0800

    add kafka client test

---
+++ src/consumer/consumer_test.go
@@ -0,0 +1,55 @@
+package consumer
+
+import (
+	"testing"
+	"time"
+
+	"gopkg.in/confluentinc/confluent-kafka-go.v1/kafka"
+)
+
+func TestKafkaTopicConsumer(t *testing.T) {
+
+	// create test producer
+	go func() {
+		p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": "kafka:9092"})
+		if err != nil {
+			t.Logf("Failed to connect to kafka broker")
+			t.Fail()
+		}
+
+		defer p.Close()
+
+		topic := "test_topic"
+		for {
+			p.Produce(&kafka.Message{
+				TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+				Value:          []byte("test_message"),
+			}, nil)
+
+			time.Sleep(1 * time.Second)
+		}
+	}()
+
+	topic_chan := make(chan *kafka.Message)
+
+	topic_consumer := KafkaTopicConsumer{
+		"test_topic",
+		topic_chan,
+		"kafka:9092",
+	}
+
+	go topic_consumer.ConsumeAndBroadcastTopics()
+
+	select {
+	case res := <-topic_chan:
+		msg := string(res.Value)
+		if msg != "test_message" {
+			t.Logf("Failed to assert topic message value")
+			t.Fail()
+		}
+	case <-time.After(1 * time.Second):
+		t.Logf("Failed to receive message from kafka")
+		t.Fail()
+	}
+
+}
