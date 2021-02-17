622fbcfd8ddaa8ba8c9cb332eb44daf27f9afaf6
pranav-geolabs <pranav@geometrylabs.io>
2021-02-16 17:04:03 -0800

    add retries to kafka consumer connection

--- src/consumer/consumer.go
+++ src/consumer/consumer.go
@@ -1,6 +1,9 @@
 package consumer
 
 import (
+	"log"
+	"time"
+
 	"gopkg.in/confluentinc/confluent-kafka-go.v1/kafka"
 )
 
@@ -13,24 +16,45 @@ type KafkaTopicConsumer struct {
 
 func (k *KafkaTopicConsumer) ConsumeAndBroadcastTopics() {
 
-	consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
-		"bootstrap.servers": k.BrokerURL,
-		"group.id":          "websocket-api-group",
-		"auto.offset.reset": "latest",
-	})
+	var consumer *kafka.Consumer
 
-	if err != nil {
-		panic(err)
+	retries := 0
+	max_retries := 60
+	for {
+		var err error
+		consumer, err = kafka.NewConsumer(&kafka.ConfigMap{
+			"bootstrap.servers": k.BrokerURL,
+			"group.id":          "websocket-api-group",
+			"auto.offset.reset": "latest",
+		})
+
+		if err != nil {
+			retries++
+
+			// Failed to connect
+			if retries >= max_retries {
+				log.Printf("Consumer: ERROR - Failed to connect to kafka broker after %d retries", max_retries)
+				panic(err)
+			}
+
+			log.Printf("Consumer: Could not connect to kafka @ %s, %d/%d retries...", k.BrokerURL, retries, max_retries)
+
+			time.Sleep(1 * time.Second)
+			continue
+		}
+
+		defer consumer.Close()
+		break
 	}
-	defer consumer.Close()
 
 	consumer.SubscribeTopics([]string{k.TopicName}, nil)
+	log.Printf("Consumer: Reading from topic %s...", k.TopicName)
 
 	for {
 		msg, err := consumer.ReadMessage(-1)
 		if err == nil {
 
-			// NOTE: use select statement for non-blocking channels
+			// Select for non-blocking channels
 			select {
 			case k.TopicChan <- msg:
 			default:
