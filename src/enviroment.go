2ac989933e265c2e425973dfa708aa7d19761006
Pranav Thiru <77865393+pranav925@users.noreply.github.com>
2021-03-09 20:24:06 -0800

    add env package to manage env (#8)
    * add env package to manage env
    
    * test

---
+++ src/enviroment.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+
+	"github.com/kelseyhightower/envconfig"
+)
+
+type Environment struct {
+	Topics     string `envconfig:"KAFKA_WEBSOCKET_SERVER_TOPICS" required:"true"`
+	BrokerURL  string `envconfig:"KAFKA_WEBSOCKET_SERVER_BROKER_URL" required:"true"`
+	Port       string `envconfig:"KAFKA_WEBSOCKET_SERVER_PORT" required:"false" default:"8080"`
+	Prefix     string `envconfig:"KAFKA_WEBSOCKET_SERVER_PREFIX" required:"false" default:""`
+	HealthPort string `envconfig:"KAFKA_WEBSOCKET_SERVER_HEALTH_PORT" required:"false" default:"5001"`
+}
+
+func getEnvironment() Environment {
+	var env Environment
+	err := envconfig.Process("", &env)
+	if err != nil {
+		log.Fatalf("ERROR: envconfig - %s\n", err.Error())
+	}
+	return env
+}
